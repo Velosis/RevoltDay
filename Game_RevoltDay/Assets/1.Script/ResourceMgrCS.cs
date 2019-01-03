18f24b0a6863fd8c6cad368796a2feee4674ed14
Velosis <joow123472@gmail.com>
2019-01-03 15:39:13 +0900

    PD
    Update

---
+++ Game_RevoltDay/Assets/1.Script/ResourceMgrCS.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class ResourceMgrCS : MonoBehaviour {
+    static public Dictionary<string, GameObject> _imgBox = new Dictionary<string, GameObject>();
+    static public Dictionary<string, GameObject> _CGImg = new Dictionary<string, GameObject>();
+    static public Dictionary<string, AudioClip> _SoundBox = new Dictionary<string, AudioClip>();
+    static public Dictionary<string, AudioClip> _BgmBox = new Dictionary<string, AudioClip>();
+
+    private void Awake()
+    {
+        object[] tempResList = null;
+        tempResList = Resources.LoadAll("1.ChrImg");
+        for (int i = 0; i < tempResList.Length; i++)
+        {
+            GameObject tempGO = tempResList[i] as GameObject;
+            _imgBox.Add(tempGO.name, tempGO);
+        }
+
+        tempResList = null;
+        tempResList = Resources.LoadAll("5.CGImg");
+        for (int i = 0; i < tempResList.Length; i++)
+        {
+            GameObject tempGO = tempResList[i] as GameObject;
+            _CGImg.Add(tempGO.name, tempGO);
+        }
+
+        tempResList = null;
+        tempResList = Resources.LoadAll("3.SoundList");
+        for (int i = 0; i < tempResList.Length; i++)
+        {
+            AudioClip tempGO = tempResList[i] as AudioClip;
+            _SoundBox.Add(tempGO.name, tempGO);
+        }
+
+        tempResList = null;
+        tempResList = Resources.LoadAll("4.BgmList");
+        for (int i = 0; i < tempResList.Length; i++)
+        {
+            AudioClip tempGO = tempResList[i] as AudioClip;
+            _BgmBox.Add(tempGO.name, tempGO);
+        }
+
+
+    }
+}
---
+++ Game_RevoltDay/Assets/1.Script/TalkCS/FlashEffCS.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlashEffCS : MonoBehaviour {
+
+    public bool _isFlashStart;
+    public float _currTime;
+
+    private float _flashOutValue;
+    private float _flashValue;
+
+    private void Awake()
+    {
+        _isFlashStart = false;
+    }
+
+    public IEnumerator RedFlashIn(float timer)
+    {
+        this.GetComponent<Image>().color = new Color(0, 0, 0, 0);
+        _flashValue = 255.0f;
+        _flashOutValue = (255.0f / timer) * 0.1f;
+
+        while (!_isFlashStart)
+        {
+            _flashValue -= _flashOutValue;
+            this.GetComponent<Image>().color = new Color(_flashValue / 255.0f, 0, 0, _flashValue / 255.0f);
+            if (_flashValue <= 0.0f) _isFlashStart = true;
+            yield return new WaitForSeconds(0.1f);
+        }
+        _flashValue = 0.0f;
+        _isFlashStart = false;
+        yield return null;
+    }
+
+    public IEnumerator WhiteFlashIn(float timer)
+    {
+        this.GetComponent<Image>().color = new Color(0, 0, 0, 0);
+        _flashValue = 255.0f;
+        _flashOutValue = (255.0f / timer) * 0.1f;
+
+        while (!_isFlashStart)
+        {
+            _flashValue -= _flashOutValue;
+            this.GetComponent<Image>().color = new Color(1, 1, 1, _flashValue / 255.0f);
+            if (_flashValue <= 0.0f) _isFlashStart = true;
+            yield return new WaitForSeconds(0.1f);
+        }
+        _flashValue = 0.0f;
+        _isFlashStart = false;
+        yield return null;
+    }
+
+}
